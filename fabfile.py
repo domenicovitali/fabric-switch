5a0c1ce768728b761d3c4095a780f42f970d7cb1
Domenico <domenico.vitali@uniroma1.it>
2018-01-24 11:57:21 +0100

    add prod

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,6 @@
-from fabric.api import run, env, roles, task, hide, open_shell, runs_once, local, settings
-from fabric.colors import red, cyan, yellow, green
-from fabric.contrib.console import confirm
-from getpass import getpass
+from fabric.api import * 
 
-from envdefs import *
-
-@task(alias="reboot")
+# @task(alias="reboot")
 def reboot():
     """
     This will save the current running configuration as the startup 
